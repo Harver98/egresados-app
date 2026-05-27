1c3f014e2243a9a09b9e32ffccb35ad3cb4fb2cb
Harver98 <haralesiegar@gmail.com>
2026-05-27 11:37:54 -0500

    Copyright 2

--- app/page.js
+++ app/page.js
@@ -427,14 +427,14 @@ export default function ConsultaPage() {
           </div>
         )}
 
-        {/* Footer */}
         <p style={styles.footer}>
           <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="#a1a1aa" strokeWidth="2" strokeLinecap="round">
             <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.15 13a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.07 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 21 17z"/>
           </svg>
-          ¿Inconvenientes? Contacta al{' '}
+          {' '}¿Inconvenientes? Contacta al{' '}
           <strong style={{ color: '#52525b', fontWeight: 600 }}>324 260 6004</strong>
-          <br>Copyright © 2026 • Diseñado y Desarrollado por Alejandro Sierra</br>
+          <br />
+          Copyright © 2026 • Diseñado y Desarrollado por Alejandro Sierra
         </p>
       </div>
 
